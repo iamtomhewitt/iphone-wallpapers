23251375bb63f2e0a60526f2f96e28f0f7d230dd
Tom Hewitt <tommhewitt@gmail.com>
2026-05-19 20:24:25 +0100

    chore: move library file so we don't accidentally invoke it elsewhere

--- src/lambda/get-contributions.ts
+++ src/lambda/get-contributions.ts
@@ -1,13 +1,48 @@
 import { withErrorHandling } from '@iamtomhewitt/error';
 
 import s3 from '../lib/s3';
-import { gitlab } from '../lib/gitlab';
 
 export const handler = withErrorHandling(
   async () => {
+    const getGitlabContributions = async (username: string, queryParameters = {}) => {
+      const perPage = 100;
+      let page = 1;
+      let all: any[] = [];
+
+      while (true) {
+        const queryParametersString = Object.entries(queryParameters)
+          .map(([key, value]) => `${key}=${value}`)
+          .join('&');
+
+        const url = `https://gitlab.com/api/v4/users/${username}/events?${queryParametersString}&per_page=${perPage}&page=${page}`;
+        const data = await fetch(url, {
+          headers: {
+            'PRIVATE-TOKEN': process.env.GITLAB_TOKEN,
+          },
+        })
+          .then(x => x.json()) as any[];
+
+        console.log('Page', page, 'has', data.length, 'events');
+
+        all = all.concat(data);
+
+        if (data.length < perPage) {
+          break;
+        }
+
+        page += 1;
+      }
+
+      return all.reduce((acc, event) => {
+        const date = event.created_at.split('T')[0];
+        acc[date] = (acc[date] || 0) + 1;
+        return acc;
+      }, {});
+    };
+
     const start = new Date();
     const thisYear = start.getFullYear();
-    const contributions = await gitlab.getContributions('thewitt_wh', {
+    const contributions = await getGitlabContributions('thewitt_wh', {
       after: `${thisYear}-01-01T00:00:00.000Z`,
     });
 
